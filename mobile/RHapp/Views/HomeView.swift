235a4b7b90a028cd9a20078b2a88393de865f183
Skyawesome73 <skyawesome73@gmail.com>
2025-03-30 00:21:43 -0500

    Add swift project

---
+++ mobile/RHapp/Views/HomeView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  RHapp
+//
+//  Created by Nicholas Puig on 3/29/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @ObservedObject var viewModel = UserViewModel()
+    @State var tabIndex: Int = 0
+    
+    var body: some View {
+        NavigationStack{
+            TabView(selection: $tabIndex) {
+                ListenView()
+                    .tabItem({
+                        Label("Home", systemImage: "house")
+                    }).tag(0)
+                DrawView()
+                    .tabItem({
+                        Label("Draw", systemImage: "paintpalette")
+                    }).tag(1)
+                LibraryView()
+                    .tabItem({
+                        Label("Your Library", systemImage: "books.vertical")
+                    }).tag(2)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("misc")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(Circle())
+                        .frame(width: 45)
+                        .overlay {
+                            Circle()
+                                .strokeBorder(.gray, lineWidth: 1)
+                    }
+                        .padding()
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("App Name")
+                        .font(Font.custom("Inter", size: 25).weight(.bold))
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationBarBackButtonHidden(true)
+        .environmentObject(viewModel)
+    }
+}
+
+#Preview {
+    HomeView().preferredColorScheme(.dark)
+}
